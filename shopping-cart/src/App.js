eec780a9a754c644748c1c7297452e9a82292a92
‘Michael <michael.daniels.jr@gmail.com>
2018-03-28 12:04:08 -0700

    items dynamically populating

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -3,22 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import CartHeader from './CartHeader';
 import CartFooter from './CartFooter';
-import CartItem from './CartItem';
+import CartItems from './CartItems';
 
 class App extends Component {
   render() {
 
     let copyrightYear = 2018;
+    let cartItemsList = [
+      { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+      { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+      { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+    ];
 
     return (
       <div>
         <CartHeader />
-        <CartItem />
-        <CartFooter year={copyrightYear}/>
+        <CartItems cartItems = {cartItemsList} />
+        <CartFooter year={copyrightYear} />
 
       </div>
     );
-    
+
   }
 }
 
--- shopping-cart/src/CartItem.js
+++ shopping-cart/src/CartItem.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const CartItem = () => {
-  return (
-    <div className="container">
-      <h1>Items</h1>
-    </div>
-  )
+
+const CartItem = (props) => {
+  console.log(props.allItems.cartItems)
+  return props.allItems.cartItems.map((item) => {
+    return (
+      <div className="list-group">
+        <div className="list-group-item">
+          <div className="row">
+            <div className="col-md-8">{item.product.name}</div>
+            <div className="col-md-2">${item.product.priceInCents.toFixed(2)}</div>
+            <div className="col-md-2">{item.quantity}</div>
+          </div>
+        </div>
+      </div>
+        )
+
+  })
 }
 
 export default CartItem;
